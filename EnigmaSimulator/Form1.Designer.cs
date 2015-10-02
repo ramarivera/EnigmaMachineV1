32bbfdd2943e476e5d84622989489c7e1421a530
RamaRivera <ramarivera@gmail.com>
2015-10-02 13:21:53 -0300

    FrontEnd de la Maquina Enigma
    Unicamente la parte grafica, no tiene funcionalidad

---
+++ EnigmaSimulator/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace EnigmaSimulator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.comboBox5 = new System.Windows.Forms.ComboBox();
+			this.comboBox6 = new System.Windows.Forms.ComboBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.comboBox7 = new System.Windows.Forms.ComboBox();
+			this.comboBox8 = new System.Windows.Forms.ComboBox();
+			this.comboBox9 = new System.Windows.Forms.ComboBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 247);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(452, 162);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Entrada";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(6, 19);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(440, 137);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(14, 72);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(157, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Posicion Inicial (Groundstellung)";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(14, 112);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(149, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Desplazamiento (Ringstellung)";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(252, 29);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(53, 21);
+			this.comboBox2.TabIndex = 8;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(311, 29);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(53, 21);
+			this.comboBox3.TabIndex = 9;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(370, 29);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(59, 20);
+			this.textBox2.TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(370, 69);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(59, 20);
+			this.textBox3.TabIndex = 14;
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Location = new System.Drawing.Point(311, 69);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(53, 21);
+			this.comboBox4.TabIndex = 13;
+			// 
+			// comboBox5
+			// 
+			this.comboBox5.FormattingEnabled = true;
+			this.comboBox5.Location = new System.Drawing.Point(252, 69);
+			this.comboBox5.Name = "comboBox5";
+			this.comboBox5.Size = new System.Drawing.Size(53, 21);
+			this.comboBox5.TabIndex = 12;
+			// 
+			// comboBox6
+			// 
+			this.comboBox6.FormattingEnabled = true;
+			this.comboBox6.Location = new System.Drawing.Point(193, 69);
+			this.comboBox6.Name = "comboBox6";
+			this.comboBox6.Size = new System.Drawing.Size(53, 21);
+			this.comboBox6.TabIndex = 11;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(370, 109);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(59, 20);
+			this.textBox4.TabIndex = 18;
+			// 
+			// comboBox7
+			// 
+			this.comboBox7.FormattingEnabled = true;
+			this.comboBox7.Location = new System.Drawing.Point(311, 109);
+			this.comboBox7.Name = "comboBox7";
+			this.comboBox7.Size = new System.Drawing.Size(53, 21);
+			this.comboBox7.TabIndex = 17;
+			// 
+			// comboBox8
+			// 
+			this.comboBox8.FormattingEnabled = true;
+			this.comboBox8.Location = new System.Drawing.Point(252, 109);
+			this.comboBox8.Name = "comboBox8";
+			this.comboBox8.Size = new System.Drawing.Size(53, 21);
+			this.comboBox8.TabIndex = 16;
+			// 
+			// comboBox9
+			// 
+			this.comboBox9.FormattingEnabled = true;
+			this.comboBox9.Location = new System.Drawing.Point(193, 109);
+			this.comboBox9.Name = "comboBox9";
+			this.comboBox9.Size = new System.Drawing.Size(53, 21);
+			this.comboBox9.TabIndex = 15;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label1);
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Controls.Add(this.textBox5);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.textBox4);
+			this.groupBox2.Controls.Add(this.comboBox7);
+			this.groupBox2.Controls.Add(this.comboBox8);
+			this.groupBox2.Controls.Add(this.comboBox9);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.comboBox4);
+			this.groupBox2.Controls.Add(this.comboBox5);
+			this.groupBox2.Controls.Add(this.comboBox6);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.comboBox3);
+			this.groupBox2.Controls.Add(this.comboBox2);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.comboBox1);
+			this.groupBox2.Location = new System.Drawing.Point(12, 29);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(452, 192);
+			this.groupBox2.TabIndex = 19;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Configuracion";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(14, 152);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(129, 13);
+			this.label3.TabIndex = 20;
+			this.label3.Text = "Conexiones (Steckerbrett)";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(193, 149);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(146, 20);
+			this.textBox5.TabIndex = 21;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(354, 147);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 22;
+			this.button1.Text = "Configurar";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.textBox6);
+			this.groupBox3.Location = new System.Drawing.Point(12, 415);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(452, 162);
+			this.groupBox3.TabIndex = 1;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Salida";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(6, 19);
+			this.textBox6.Multiline = true;
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(440, 137);
+			this.textBox6.TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(193, 29);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(53, 21);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(141, 13);
+			this.label1.TabIndex = 23;
+			this.label1.Text = "Orden Rotores (Walzenlage)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(486, 589);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(502, 628);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(502, 628);
+			this.Name = "Form1";
+			this.Text = "Enigma Simulator";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.ComboBox comboBox4;
+		private System.Windows.Forms.ComboBox comboBox5;
+		private System.Windows.Forms.ComboBox comboBox6;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.ComboBox comboBox7;
+		private System.Windows.Forms.ComboBox comboBox8;
+		private System.Windows.Forms.ComboBox comboBox9;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
